eb43ea872960a2c9b6d9830d709a4c83ea81a14d
Giancarlo Eje <giancarloeje@gmail.com>
2020-04-28 18:43:31 +0800

    CMOSD-1854 self.save! should honor "Display error if stale" option

--- lib/mongoid/relations_dirty_tracking.rb
+++ lib/mongoid/relations_dirty_tracking.rb
@@ -36,12 +36,12 @@ module Mongoid
 
     def transform_changes_by_type(o)
       case o
-        when Hash
-          transform_hash(o)
-        when Array
-          transform_array(o)
-        else
-          o
+      when Hash
+        transform_hash(o)
+      when Array
+        transform_array(o)
+      else
+        o
       end
     end
 
@@ -80,6 +80,9 @@ module Mongoid
       changes or relations_changed?
     end
 
+    def restore_changes
+      @restore_changes = (changes || {}).merge previous_changes unless previous_changes.empty?
+    end
 
     def tracked_relation_attributes(rel_name)
       rel_name = rel_name.to_s
@@ -106,6 +109,14 @@ module Mongoid
       values
     end
 
+    def changes
+      return super if @restore_changes.nil?
+      @restore_changes
+    end
+
+    def remove_change(name)
+      changes.delete(name.to_s)
+    end
 
     module ClassMethods
 
